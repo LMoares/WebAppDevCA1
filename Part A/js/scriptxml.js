001791eefa8d7783b0cf8e19da42918925d752b3
lmoares <moares.lorenzo@gmail.com>
2025-10-16 15:44:09 +0100

    altered product search to be handled via hashmap, added product comparison, fixed logic in search, reset, compare, and print, general comment additions - 16/10/2025

--- Part A/js/scriptxml.js
+++ Part A/js/scriptxml.js
@@ -1,97 +1,147 @@
 let xml;
 let xsl;
-let output = document.getElementById("searchOutput");
+let data = new Map();
+//data will store products from xml
+//key: product code, value: array of product child elements
+//eg: "001-01", [name, description, quantity, price]
+let compareList = [];
+//output elements for html page
+let searchMessage = document.getElementById("searchMessage");
+let searchOutput = document.getElementById("searchOutput");
+let compareOutput = document.getElementById("compareOutput");
 
 function searchProduct() {
+  //input
+  //take value from search bar and remove whitespace
+  let code = document.getElementById("searchInput").value.trim();
+
+  //determine product is defined
+  if (data.has(code)) {
+    searchOutput.innerHTML = printProduct(code);
+  } else if (code === "") {
+    let error = `<p class="text-center text-danger">Search requires Code. (eg: 101-01)</p>`;
+    searchMessage.innerHTML = error;
+  } else {
+    //code not empty and code not found
+    let error = `<p class="text-center text-danger">No product found with Code: ${code}</p>`;
+    searchMessage.innerHTML = error;
+  }
+}
+
+function resetProducts() {
+  //reset all fields to empty strings
+  searchOutput.innerHTML = "";
+  searchMessage.innerHTML = "";
+  document.getElementById("searchInput").value = "";
+  compareOutput.innerHTML = "";
+  document.getElementById("compareHeader").innerHTML = "";
+  compareList = [];
+}
+
+function compareProducts() {
   let code = document.getElementById("searchInput").value.trim(); //take value from search bar and remove whitespace
+  //determine if code is valid and product not already in list
+  if (!compareList.includes(code) && data.has(code)) {
+    compareList.push(code); //add current code to list of compared products - ensures unique products only in list
 
-  let products = xml.getElementsByTagName("product");
-  let found = false;
+    document.getElementById(
+      "compareHeader"
+    ).innerHTML = `<hr/><h5>Compared Products</h5>`; //create compared product header
 
-  //iterate through all products from xml
-  for (let product of products) {
-    //product associated with code found
-    if (product.getAttribute("code") === code) {
-      found = true;
+    compareOutput.innerHTML += printProduct(code) + `<br/>`; //add product card to compare list
 
-      //intialize local variables to text stored in product child elements
-      let name = product.getElementsByTagName("name")[0].textContent;
-      let desc = product.getElementsByTagName("description")[0].textContent;
-      let qnty = product.getElementsByTagName("quantity")[0].textContent;
-      let price = product.getElementsByTagName("unit_price")[0].textContent;
+    searchMessage.innerHTML = ""; //clear error messages on successful query
+  } else if (!data.has(code)) {
+    //if code not valid error
+    let error = `<p class="text-center text-danger">Product: ${code} not found</p>`;
+    searchMessage.innerHTML = error;
+  } else {
+    //product already in list
+    let error = `<p class="text-center text-danger">Product: ${code} already in Compare List</p>`;
+    searchMessage.innerHTML = error;
+  }
+}
 
-      //markup for card html
-      let card = `
+function printProduct(code) {
+  //card markup that includes specific product information - built using data map
+  let card = `
         <div class="d-flex justify-content-center">
           <div class="card index_card w-100">
             <div class="card-body d-flex flex-column justify-content-center">
               <span class="card-title text-center">
-                ${name}<br />
+                ${data.get(code)[0]}<br />
                 <b>${code}</b>
               </span>
-              <ul>
-                <li><b>Description:</b> ${desc}</li>
-                <li><b>Quantity:</b> ${qnty}</li>
-                <li><b>Unit Price:</b> $${price}</li>
+              <ul class="text-start">
+                <li><b>Description:</b> ${data.get(code)[1]}</li>
+                <li><b>Quantity:</b> ${data.get(code)[2]}</li>
+                <li><b>Unit Price:</b> $${data.get(code)[3]}</li>
               </ul>
             </div>
           </div>
         </div>`;
 
-      //set innerHTML to card markup
-      output.innerHTML = card;
+  return card;
+}
 
-      break; //product found early, exit loop
-    }
-  }
+function buildDataMap() {
+  let products = xml.getElementsByTagName("product");
 
-  if (code === "") {
-    let error = `<p class="text-center text-danger">Search requires Code. (eg: 101-01)</p>`;
-    output.innerHTML = error;
-  } else if (!found) {
-    let error = `<p class="text-center text-danger">No product found with Code: ${code}</p>`;
-    output.innerHTML = error;
+  //iterate through all products from xml
+  for (let product of products) {
+    //add elements to data dictionary - key: product code, value: array of product elements
+    data.set(product.getAttribute("code"), [
+      product.getElementsByTagName("name")[0].textContent,
+      product.getElementsByTagName("description")[0].textContent,
+      product.getElementsByTagName("quantity")[0].textContent,
+      product.getElementsByTagName("unit_price")[0].textContent,
+    ]);
   }
 }
 
-function resetProducts() {
-  output.innerHTML = "";
-}
-
-function getXMLXSL() {
+function getXMLXSLData() {
+  //relative xml/xsl path
   const xmlPath = "XML/products.xml";
   const xslPath = "XML/products.xsl";
 
   const fetchXML = new XMLHttpRequest();
+  //build request for xml data
   fetchXML.open("GET", xmlPath, false);
+  //send above request from browser to server - waits for response
   fetchXML.send();
-
+  //captures response and saves it to local variable
   const xmlFile = fetchXML.responseXML;
 
+  //same as above but for xsl file
   const fetchXSL = new XMLHttpRequest();
   fetchXSL.open("GET", xslPath, false);
   fetchXSL.send();
-
   const xslFile = fetchXSL.responseXML;
 
+  //returns the xml and xsl data as strings
   return [xmlFile, xslFile];
 }
 
-function loadXMLXSL() {
-  [xml, xsl] = getXMLXSL();
-  console.log(xml);
-  console.log(xsl);
+function loadXMLXSLToPage() {
+  //retrieves xml and xsl data as strings
+  [xml, xsl] = getXMLXSLData();
 
-  console.log(window.location.href);
+  //checks to ensure xml and xsl data has been loaded, posts error to console if not
   if (!xml || !xsl) {
     console.error("XML or XSL failed to load.");
     return;
   }
-  const xsltProcessor = new XSLTProcessor();
-  xsltProcessor.importStylesheet(xsl);
 
-  const resultDocument = xsltProcessor.transformToFragment(xml, document);
-  document.getElementById("output").appendChild(resultDocument);
+  //processes xsl rules and styles xml
+  const processor = new XSLTProcessor();
+  processor.importStylesheet(xsl);
+  const transformedDoc = processor.transformToFragment(xml, document);
+  //appends transformed xml data to outputXML html element following template from xsl
+  document.getElementById("outputXML").appendChild(transformedDoc);
+
+  //adds all product information to hashmap for constant time lookup of product details
+  buildDataMap();
 }
 
-document.addEventListener("DOMContentLoaded", loadXMLXSL, false);
+//on page load run loadXMLXSLToPage function
+document.addEventListener("DOMContentLoaded", loadXMLXSLToPage, false);
