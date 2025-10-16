0d2552318a24bc08bce8a68dc1d471a40cf8b66c
lmoares <moares.lorenzo@gmail.com>
2025-10-16 13:44:28 +0100

    section 1 completed. may require second pass - 16/10/2025

--- Part A/XML/products.xsl
+++ Part A/XML/products.xsl
@@ -2,23 +2,18 @@
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
     <xsl:output method="html"/>
     <xsl:template match="/category">
-        <div>
-            <h2 class=" pt-5 text-center">Soft Drinks</h2>
+        <div class="container">
+            
             <xsl:for-each select="product">
-            <div class="my-5 mx-5 d-flex flex-row justify-content-center align-items-center">
-                <div class="card index_card">
-                    <div class="card-body d-flex flex-column justify-content-center">
-                        <span class="card-title text-center"><xsl:value-of select="name"/> <br/>CODE: <b><xsl:value-of select="@code"/></b></span>
+                <div class="d-flex justify-content-center mb-5">
+                    <div class="card index_card w-100">
+                        <div class="card-body d-flex flex-column justify-content-center">
+                            <span class="card-title text-center"><xsl:value-of select="name" /><br />
+                            CODE: <b><xsl:value-of select="@code" /></b></span>
                             <ul>
-                                <li> 
-                                    <b>Description: </b> <xsl:value-of select="description"/>
-                                </li>
-                                <li> 
-                                    <b>Quantity: </b> <xsl:value-of select="quantity"/>
-                                </li>
-                                <li> 
-                                    <b>Unit Price: </b> $<xsl:value-of select="unit_price"/>
-                                </li>
+                                <li><b>Description: </b> <xsl:value-of select="description" /></li>
+                                <li><b>Quantity: </b> <xsl:value-of select="quantity" /></li>
+                                <li><b>Unit Price: </b> $<xsl:value-of select="unit_price" /></li>
                             </ul>
                         </div>
                     </div>
--- Part A/js/scriptxml.js
+++ Part A/js/scriptxml.js
@@ -1,3 +1,63 @@
+let xml;
+let xsl;
+let output = document.getElementById("searchOutput");
+
+function searchProduct() {
+  let code = document.getElementById("searchInput").value.trim(); //take value from search bar and remove whitespace
+
+  let products = xml.getElementsByTagName("product");
+  let found = false;
+
+  //iterate through all products from xml
+  for (let product of products) {
+    //product associated with code found
+    if (product.getAttribute("code") === code) {
+      found = true;
+
+      //intialize local variables to text stored in product child elements
+      let name = product.getElementsByTagName("name")[0].textContent;
+      let desc = product.getElementsByTagName("description")[0].textContent;
+      let qnty = product.getElementsByTagName("quantity")[0].textContent;
+      let price = product.getElementsByTagName("unit_price")[0].textContent;
+
+      //markup for card html
+      let card = `
+        <div class="d-flex justify-content-center">
+          <div class="card index_card w-100">
+            <div class="card-body d-flex flex-column justify-content-center">
+              <span class="card-title text-center">
+                ${name}<br />
+                <b>${code}</b>
+              </span>
+              <ul>
+                <li><b>Description:</b> ${desc}</li>
+                <li><b>Quantity:</b> ${qnty}</li>
+                <li><b>Unit Price:</b> $${price}</li>
+              </ul>
+            </div>
+          </div>
+        </div>`;
+
+      //set innerHTML to card markup
+      output.innerHTML = card;
+
+      break; //product found early, exit loop
+    }
+  }
+
+  if (code === "") {
+    let error = `<p class="text-center text-danger">Search requires Code. (eg: 101-01)</p>`;
+    output.innerHTML = error;
+  } else if (!found) {
+    let error = `<p class="text-center text-danger">No product found with Code: ${code}</p>`;
+    output.innerHTML = error;
+  }
+}
+
+function resetProducts() {
+  output.innerHTML = "";
+}
+
 function getXMLXSL() {
   const xmlPath = "XML/products.xml";
   const xslPath = "XML/products.xsl";
@@ -18,7 +78,7 @@ function getXMLXSL() {
 }
 
 function loadXMLXSL() {
-  const [xml, xsl] = getXMLXSL();
+  [xml, xsl] = getXMLXSL();
   console.log(xml);
   console.log(xsl);
 
