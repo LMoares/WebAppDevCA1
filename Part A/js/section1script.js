eeba6fe0261600e8d90b915933c7a293e4dbe841
Lorenzo <moares.lorenzo@gmail.com>
2025-10-19 14:03:35 +0100

    section 1 finalized - 19/10/2025

--- Part A/js/section1script.js
+++ Part A/js/section1script.js
@@ -4,7 +4,7 @@ let xsl;
 let data = new Map();
 //data will store products from xml
 //key: product code, value: array of product child elements
-//eg: "001-01", [name, description, quantity, price]
+//e.g. "001-01", [name, description, quantity, price]
 
 let compareList = []; //array to hold product codes to ensure user only receives unique products in comparison view
 let productCode; //product code set by successful search query, used to add product to compared products list
@@ -19,19 +19,26 @@ function searchProduct() {
   //take value from search bar and remove whitespace
   productCode = document.getElementById("searchInput").value.trim();
 
-  //determine product is defined
   if (data.has(productCode)) {
+    //determine product is defined
     searchOutput.innerHTML = printProduct(productCode);
     searchMessage.innerHTML = ""; //reset error message on successful query
     document.getElementById("compareBtn").className = "btn btn-primary mt-3"; //reveals compare button for user
     document.getElementById("searchInput").value = ""; //reset search bar text
+  } else if (productCode.match(/^\d{3}-\d{2}$/)) {
+    //regular expression ^: start of string, \d{3}: 3 digits, -: dash, \d{2}: 2 digits, $: end of string
+    let error = `<p class="text-center text-light bg-danger d-inline-block px-2 rounded">Error: No product found with Code: ${productCode}</p>`;
+    searchMessage.innerHTML = error;
+    document.getElementById("searchInput").value = ""; //reset search bar text
   } else if (productCode === "") {
-    let error = `<p class="text-center text-light bg-danger d-inline-block px-2">Search requires Code. (eg: 101-01)</p>`;
+    //ensures search is not empty
+    let error = `<p class="text-center text-light bg-danger d-inline-block px-2 rounded">Error: Search requires Code. (eg: 101-01)</p>`;
     searchMessage.innerHTML = error;
   } else {
     //code not empty and code not found
-    let error = `<p class="text-center text-light bg-danger d-inline-block px-2">No product found with Code: ${productCode}</p>`;
+    let error = `<p class="text-center text-light bg-danger d-inline-block px-2 rounded">Error: Code must be ###-## where # is a digit</p>`;
     searchMessage.innerHTML = error;
+    document.getElementById("searchInput").value = ""; //reset search bar text
   }
 }
 
@@ -41,34 +48,28 @@ function resetProducts() {
   searchMessage.innerHTML = "";
   document.getElementById("searchInput").value = "";
   compareOutput.innerHTML = "";
-  document.getElementById("compareHeader").innerHTML = "";
+  document.getElementById("compareHeader").className = "mt-4 d-none"; //hide compare header
   document.getElementById("compareBtn").className =
     "btn btn-primary mt-3 d-none"; //hides compare button
-  compareList = [];
+  compareList = []; //compared products array set to empty
 }
 
 function compareProducts() {
+  document.getElementById("compareHeader").className = "mt-4"; //display compared products header
+
   if (!compareList.includes(productCode)) {
     //determine if product already exists in compare list
-    compareList.push(productCode); //add current code to list of compared products - ensures unique products only in list
-
-    document.getElementById(
-      "compareHeader"
-    ).innerHTML = `<hr/><h3>Compared Products</h3>`; //create compared product header
-
+    compareList.push(productCode); //add current code to list of compared products
     compareOutput.innerHTML += printProduct(productCode) + `<br/>`; //add product card to compare list
 
     document.getElementById("compareBtn").className =
-      "btn btn-primary mt-3 d-none"; //hides compare button
-    searchMessage.innerHTML = `<p class="text-center text-light bg-success d-inline-block px-2">Product Code: ${productCode} - added to Compare List</p>`; //informs user of product information moved to compare list
-    searchOutput.innerHTML = ""; //clear search output on product appearing in compare list
-  } else if (!data.has(productCode)) {
-    //if code not valid error
-    let error = `<p class="text-center text-danger">Product: ${productCode} not found</p>`;
-    searchMessage.innerHTML = error;
+      "btn btn-primary mt-3 d-none"; //hide compare button
+
+    searchMessage.innerHTML = `<p class="text-center text-light bg-success d-inline-block px-2 rounded">Product Code: ${productCode} - added to Compare List</p>`; //informs user of product information moved to compare list
+    searchOutput.innerHTML = ""; //clear search output when product moved to compare list
   } else {
     //product already in list
-    let error = `<p class="text-center text-danger">Product: ${productCode} already in Compare List</p>`;
+    let error = `<p class="text-center text-light bg-danger d-inline-block px-2 rounded">Error: Product: ${productCode} - already in Compare List</p>`;
     searchMessage.innerHTML = error;
   }
 }
---
+++ Part A/js/section2script.js
@@ -0,0 +1,1 @@
+const test = { city: "Dublin", temperature: 12, humidity: 98 };
